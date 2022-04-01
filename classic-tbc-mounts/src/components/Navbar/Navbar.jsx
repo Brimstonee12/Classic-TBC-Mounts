4c940c201fbdb1cdb884f39db1958168bbe1eed6
Brimstonee12 <brimstonee12@gmail.com>
2022-04-01 21:52:15 +0200

    Added working navigation links

--- classic-tbc-mounts/src/components/Navbar/Navbar.jsx
+++ classic-tbc-mounts/src/components/Navbar/Navbar.jsx
@@ -1,17 +1,48 @@
-import './NavbarStyles.css';
-import { Link } from "react-router-dom";
+import "./NavbarStyles.css";
+import { NavLink } from "react-router-dom";
+import logo from "../../assets/wow_tbc.webp";
 
 export const Navbar = () => {
+  const navbarLinksData = [
+    {
+      link: "/",
+      title: "Classic Easy Mounts",
+    },
+    {
+      link: "/classic-easy-mounts",
+      title: "Classic Rare Mounts",
+    },
+    {
+      link: "/classic-rare-mounts",
+      title: "TBC Easy Mounts",
+    },
+    {
+      link: "/tbc-easy-mounts",
+      title: "TBC Rare Mounts",
+    },
+    {
+      link: "/tbc-rare-mounts",
+      title: "Vendor Mounts",
+    },
+  ];
+
   return (
     <div className="navbarContainer">
       <div className="navbarLogo">
-
+        <img width="250" height="160" src={logo} />
       </div>
-      <div className="navbarLinks">
-
-</div>
+      <div className="navbarLinksContainer">
+        {navbarLinksData.map((linkData) => (
+          <NavLink
+            to={linkData.link}
+            className={({ isActive }) =>
+              isActive ? "navbarLinkActive" : "navbarLink"
+            }
+          >
+            <h4 className="navbarTitle">{linkData.title}</h4>
+          </NavLink>
+        ))}
+      </div>
     </div>
   );
-}
-
-
+};
